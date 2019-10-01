94ede847ec65523951773d27b5b9054ef678a35c
Michael West <mwester1111@gmail.com>
2019-09-30 20:34:52 -0400

    Spaghetti and stupid practices fixes
    Added some new functions, worked on making it more modular, created scanner and capture subclasses

--- sharkBot.py
+++ sharkBot.py
@@ -4,55 +4,68 @@ import seaborn as sns
 import pandas as pd
 import geoip2.database as geo
 import geoip2
-class sharkBot:
-	def capturePacketsByCount(self, interface, packet_count, tshark_path, output_file=None):
+import asyncio
+class scanner:
+	def capturePacketsByCount(self, interface="en0", packet_count=100, tshark_path="/Applications/Wireshark.app/Contents/MacOS/tshark", output_file=None):
 		#capture specified number of packets 
 		self.capture = pyshark.LiveCapture(tshark_path=tshark_path, interface=interface, output_file=output_file)
 		self.capture.sniff(packet_count=packet_count)
 		self.caplen = len(self.capture)
+		#capture packets for given amount of time
+		return self.capture
+	def readCaptureFile(self, filename):
+		self.capture = pyshark.FileCapture(filename)
+		self.caplen = len(self.capture)
+		return self.capture
+	def getInfo(self, capture):
 		#Create lists for storing IP addresses
 		i = 0
 		self.dst = list()
 		self.src = list()
 		self.packetswithips = list()
+		self.caplen = len(capture)
 		#Go through every packet, and if it has an IP address add it to the lists
 		while (i < self.caplen):
-			ip1 = self.capture[i]
+			ip1 = capture[i]
 			if 'IP' in ip1:
 				self.dst.append(ip1['ip'].dst)
 				self.src.append(ip1['ip'].src)
 				self.packetswithips.append(i)
 			i += 1
-		#capture packets for given amount of time
-		return self.capture
-	def capturePacketsByTime(self, interface, timeout, tshark_path, output_file=None):
+		#Create lists for storing UDP information
+		i = 0
+		self.chcksum = list()
+		self.dstport = list()
+		self.port = list()
+		self.srcport = list()
+		self.packetswithudp = list()
+		#Go through every packet, and if it has an IP address add it to the lists
+		while (i < self.caplen):
+			udp1 = capture[i]
+			if 'UDP' in udp1:
+				self.dstport.append(udp1['udp'].dstport)
+				self.srcport.append(udp1['udp'].srcport)
+				self.port.append(udp1['udp'].port)
+				self.chcksum.append(udp1['udp'].checksum)
+				self.packetswithudp.append(i)
+			i += 1
+	def capturePacketsByTime(self, interface, timeout, tshark_path=None, output_file=None):
 		self.capture = pyshark.LiveCapture(tshark_path=tshark_path, interface=interface, output_file=output_file)
 		self.capture.sniff(timeout=timeout)
 		self.caplen = len(self.capture)
-		#Create lists for storing IP addresses
-		i = 0
-		self.dst = list()
-		self.src = list()
-		self.packetswithips = list()
-		#Go through every packet, and if it has an IP address add it to the lists
-		while (i < self.caplen):
-			ip1 = self.capture[i]
-			if 'IP' in ip1:
-				self.dst.append(ip1['ip'].dst)
-				self.src.append(ip1['ip'].src)
-				self.packetswithips.append(i)
-			i += 1
 		return self.capture
-	def plot_AverageLenOfLayer(self):
+class capture:
+	def plot_AverageLenOfLayer(self, capture):
 		i = 0
 		#Create a Datframe and lists to store our values that we read from the packet
 		df = pd.DataFrame()
 		layer = list()
 		#Create a dictionary that will store our average values by layer
 		averages = {}
+		caplen = len(capture)
 		#Loop that steps through each packet and grabs all unique layers and each individual length value
-		while (i < self.caplen):
-			dns1 = self.capture[i]
+		while (i < caplen):
+			dns1 = capture[i]
 			#This loop checks to see if the layer has already been added to the layers list, if not it adds it
 			if dns1.highest_layer not in layer:
 				layer.append(dns1.highest_layer)
@@ -60,8 +73,8 @@ class sharkBot:
 			i += 1
 		i = 0
 		#This loop creates a column for each layer and then adds it's respective length values
-		while (i < self.caplen):
-			dns1 = self.capture[i]
+		while (i < caplen):
+			dns1 = capture[i]
 			df = df.append({dns1.highest_layer: int(dns1.length)}, ignore_index=True)
 			i += 1
 		#This loop gets the average of each column then adds it to a dictionary with the corresponding layer value to plot later
@@ -72,45 +85,28 @@ class sharkBot:
 		#The X labels are determined by the labels of the layers, also known as the keys
 		#Example: {'MDNS': 450} would plot a bar going to a y value of 450 with the label on the X axis being MDNS
 		plt.xticks(range(len(averages)), list(averages.keys()))
-		plt.show()
-	def listOfIPs(self, filename, capnum):
+		fname = "Len_Of_Layer.png"
+		return plt.save(fname)
+	def listOfIPs(self, filename, ):
 		i = 0
 		#Write the IPs for each packet to a .txt file for future use
 		while (i < len(self.src)):
 			f = open(filename, "a")
-			towrite = "Capture: " + str(capnum) + "\n" + "Packet Number: " + str(self.packetswithips[i]) + "\n    " + "Source IP: " + self.src[i] + "\n    " + "Destination IP: " + self.dst[i] + "\n"
+			towrite = "Capture: " + str(self.caplen) + "\n" + "Packet Number: " + str(self.packetswithips[i]) + "\n    " + "Source IP: " + self.src[i] + "\n    " + "Destination IP: " + self.dst[i] + "\n"
 			f.write(towrite)
 			f.close()
 			i += 1
-	def getProtocol(self, packetnum):
-		if 'protocol' in  self.capture[packetnum]:
-			protocol = self.capture[packetnum]['protocol']
+	def getProtocol(self, capture, packetnum):
+		if 'protocol' in capture[packetnum]:
+			protocol = capture[packetnum]['protocol']
 			print(protocol)
 		else:
 			print("Error: packet has no attribute 'protocol'")
 	def listOfUDPs(self, filename, capnum):
-		#Create lists for storing UDP information
-		i = 0
-		chcksum = list()
-		dstport = list()
-		port = list()
-		srcport = list()
-		packetswithudp = list()
-		#Go through every packet, and if it has an IP address add it to the lists
-		while (i < self.caplen):
-			udp1 = self.capture[i]
-			if 'UDP' in udp1:
-				dstport.append(udp1['udp'].dstport)
-				srcport.append(udp1['udp'].srcport)
-				port.append(udp1['udp'].port)
-				chcksum.append(udp1['udp'].checksum)
-				packetswithudp.append(i)
-			i += 1
-		i = 0
-		#Write the IPs for each packet to a .txt file for future use
-		while (i < len(packetswithudp)):
+		#Write the UDPs for each packet to a .txt file for future use
+		while (i < len(self.packetswithudp)):
 			f = open(filename, "a")
-			towrite = "Capture: " + str(capnum) + "\n" + "Packet Number: " + str(packetswithudp[i]) + "\n    " + "Checksum: " + chcksum[i]  + "\n    " + "Port: " + port[i] +  "\n    " + "Source Port: " + srcport[i] + "\n    " + "Destination Port: " + dstport[i] + "\n"
+			towrite = "Capture: " + str(capnum) + "\n" + "Packet Number: " + str(self.packetswithudp[i]) + "\n    " + "Checksum: " + chcksum[i]  + "\n    " + "Port: " + port[i] +  "\n    " + "Source Port: " + srcport[i] + "\n    " + "Destination Port: " + dstport[i] + "\n"
 			f.write(towrite)
 			f.close()
 			i += 1
@@ -140,11 +136,44 @@ class sharkBot:
 			except geoip2.errors.AddressNotFoundError as error:
 				pass
 		f.close()
-	def DNSList(self, filename):
+	def SecurityResponse(self, capture):
+		#A loop that for each source ip, checks if it is in the country database then returns lists that we can spit out on the discord
+		#Create lists for storing IP addresses
+		i = 0
+		dst = list()
+		src = list()
+		caplen = len(capture)
+		#Go through every packet, and if it has an IP address add it to the lists
+		while (i < caplen):
+			ip1 = capture[i]
+			if 'IP' in ip1:
+				dst.append(ip1['ip'].dst)
+				src.append(ip1['ip'].src)
+			i += 1
+		reader = geo.Reader('/Users/cooldude/Downloads/GeoLite2-Country_20190430/GeoLite2-Country.mmdb')
+		foreigns = []
+		for ip in src:
+			#If IP is found, print our associated country
+			try:
+				response = reader.country(ip)
+				if response.country.name != 'United States':
+					foreigns.append("Source: " + ip + ": " + response.country.name + "\n")
+			#If country is not found, just keep going
+			except geoip2.errors.AddressNotFoundError as error:
+				pass
+		#A loop that for each destination ip, checks if it is in the country database
+		for ip in dst:
+			#If IP is found, print our associated country
+			try:
+				response = reader.country(ip)
+				if response.country.name != 'United States':
+					foreigns.append("Destination: " + ip + response.country.name + "\n")
+			#If country is not found, just keep going
+			except geoip2.errors.AddressNotFoundError as error:
+				pass
+		return foreigns
+	def DNSList(self, filename, capture):
 		f = open(filename, "a")
-		for packet in self.capture:
+		for packet in capture:
 			if packet.dns.qry_name in packet:
 				f.write("DNS:", packet.dns.qry_name)
-bot = sharkBot()
-bot.capturePacketsByCount(interface="en0", tshark_path="/Applications/Wireshark.app/Contents/MacOS/tshark", packet_count=250)
-bot.plot_AverageLenOfLayer()
